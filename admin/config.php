ed22622e29089c8f6ce549e274cfd92066792320
Joshua Vicente <josh.vicente00@gmail.com>
2024-06-02 23:35:23 +0800

    sample push

--- admin/config.php
+++ admin/config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "phoneshop";
+$dbname = "phoneshop";  //comment here 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
